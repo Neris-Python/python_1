4dbedcc079db916f8bd0d3a69a2a8d379262a91d
Neris-Python <129526731+Neris-Python@users.noreply.github.com>
2025-09-29 22:43:16 +0300

    Update task_2.py
    Задание 2 без использования цикла

--- task_2.py
+++ task_2.py
@@ -1,7 +1,12 @@
 string = input("Введите строку: ")
-vowels = 'aeiouAEIOU'
-result = ''
-for letter in string:
-    if letter not in vowels:
-        result += letter
-print("Строка без гласных:", result)
+string = string.replace('a', '')
+string = string.replace('e', '')
+string = string.replace('i', '')
+string = string.replace('o', '')
+string = string.replace('u', '')
+string = string.replace('A', '')
+string = string.replace('E', '')
+string = string.replace('I', '')
+string = string.replace('O', '')
+string = string.replace('U', '')
+print("Строка без гласных:", string)
