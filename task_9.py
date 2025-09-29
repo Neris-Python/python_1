b37b285071a4661622bad5c7e7500c3d416008cd
Neris-Python <129526731+Neris-Python@users.noreply.github.com>
2025-09-29 22:49:38 +0300

    Update task_9.py
    Задание 9 без использования цикла

--- task_9.py
+++ task_9.py
@@ -1,19 +1,15 @@
 ip = input("Введите IP-адрес: ")
 parts = ip.split('.')
+
 if len(parts) != 4:
     print("Не корректный IP!")
 else:
-    point= True
-    for part in parts:
-        if not part.isdigit():
-            point = False
-            break
-        num = int(part)
-        if num < 0 or num > 255:
-            point = False
-            break
-
-    if point:
-        print("Корректный IP-адрес!")
+    p1, p2, p3, p4 = parts
+    if (p1.isdigit() and p2.isdigit() and p3.isdigit() and p4.isdigit()):
+        n1, n2, n3, n4 = int(p1), int(p2), int(p3), int(p4)
+        if 0 <= n1 <= 255 and 0 <= n2 <= 255 and 0 <= n3 <= 255 and 0 <= n4 <= 255:
+            print("Корректный IP-адрес!")
+        else:
+            print("Не корректный IP!")
     else:
         print("Не корректный IP!")
